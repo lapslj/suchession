dd2cdab0a3fd4be8733faf69c5e940b0182cce38
John <john'semail>
2022-10-30 21:40:28 -0400

    moving pieces

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const letters = document.querySelectorAll('.box');
-
+const pieces = document.querySelectorAll('.piece');
 const body = document.querySelectorAll(".white");
 const docbody = document.querySelector("body");
 
@@ -25,3 +25,35 @@ docbody.addEventListener("click",function(event){
     makeBody(bgColoRandom());
     console.log(event.pageX);
 })
+
+boxArray = Array.from(letters)
+let targetPiece = document.querySelector("img")
+
+function classConvert(boxClass,pieceClass){
+    strArray = Array.from(boxClass);
+    sFirst = strArray[0];
+    sLast = strArray[2];
+    return `c${sFirst}${sLast}`
+}
+
+function onBoxClick(e,thisClass,targetPiece){
+    newClass = e.target.getAttribute("id");
+    console.log(newClass);
+    pieceClass = classConvert(newClass);
+    targetPiece.setAttribute("id",pieceClass);
+    //in this function we need to change the parent element's class to match thisClass
+}
+
+
+for(let piece of pieces){
+    piece.addEventListener("click",function(event){
+        thisClass = event.target.getAttribute("id");
+        targetPiece = event.target;
+        console.log(thisClass);
+        for(let box of boxArray){
+            box.addEventListener("click",function(e){
+                onBoxClick(e,thisClass,targetPiece)})
+        }
+
+    })
+}
